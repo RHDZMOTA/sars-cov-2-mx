9de715531b8a4b0e66436d60a5c8969a2dbb0ed7
rhdzmota <rhdzmota@gmail.com>
2022-01-29 00:57:51 -0600

    Parse environ variables

--- sars_cov_2_mx/settings.py
+++ sars_cov_2_mx/settings.py
@@ -2,9 +2,9 @@ import os
 
 
 
-COV_TOKEN = os.environ.get("COV_TOKEN")
-COV_GIST_ID = os.environ.get("COV_GIST_ID")
-COV_FILENAME = os.environ.get("COV_FILENAME")
+COV_TOKEN = os.environ.get("COV_TOKEN", "").strip() or None
+COV_GIST_ID = os.environ.get("COV_GIST_ID", "").strip() or None
+COV_FILENAME = os.environ.get("COV_FILENAME", "").strip() or None
 
 URL_TEMPLATE = "https://datos.covid-19.conacyt.mx/Downloads/Files/Casos_Diarios_Estado_Nacional_Confirmados_{date}.csv"
 URL_GIST = "https://gist.githubusercontent.com/RHDZMOTA/{gist_id}/raw/{filename}".format(
