b38458786353971119cb381918374f4748c3693d
rhdzmota <rhdzmota@gmail.com>
2022-01-30 17:16:11 -0600

    Create custom logging

--- sars_cov_2_mx/settings.py
+++ sars_cov_2_mx/settings.py
@@ -1,5 +1,7 @@
 import os
-
+import logging
+from logging.config import dictConfig
+from typing import Optional, Dict
 
 
 COV_TOKEN = os.environ.get("COV_TOKEN", "").strip() or None
@@ -11,3 +13,46 @@ URL_GIST = "https://gist.githubusercontent.com/RHDZMOTA/{gist_id}/raw/{filename}
     gist_id=COV_GIST_ID,
     filename=COV_FILENAME
 )
+
+COV_DEFAULT_LOGLEVEL = os.environ.get(
+    "COV_DEFAULT_LOGLEVEL",
+    default="INFO"
+).upper()
+
+COV_DISABLE_EXISTING_LOGGERS = os.environ.get(
+    "COV_DISABLE_EXISTING_LOGGERS",
+    default="FALSE"
+).upper().startswith("T")
+
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": COV_DISABLE_EXISTING_LOGGERS,
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+        },
+    },
+    "handlers": {
+        "default": {
+            "level": COV_DEFAULT_LOGLEVEL,
+            "formatter": "standard",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": [
+                "default"
+            ],
+            "level": COV_DEFAULT_LOGLEVEL,
+            "propagate": True,
+        }
+    }
+}
+
+
+def get_logger(name: str, logging_config_dictionary: Optional[Dict] = None):
+    if logging_config_dictionary is None:
+        logging_config_dictionary = logging_config
+    dictConfig(logging_config_dictionary)
+    return logging.getLogger(name)
